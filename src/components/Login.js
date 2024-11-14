46c5ca950286f7642b74745b5cd45f4fb1040fb6
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-14 20:01:01 +0530

    Refactor Login component layout and update form fields for improved user experience

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,11 +11,12 @@ const Login = () => {
       </div>
 
       
-        <form className='relative p-12 bg-black  '>
-            <h4>Sign In</h4>
-            <input type="text" className='p-4 m-4' />
-            <input type="text" className='p-4 m-4' />
-            <button type="button" className='p-4 m-4' >Sign In</button>
+        <form className='absolute gap-4 text-white p-12 bg-black w-3/12 my-36 flex flex-wrap flex-col bg-opacity-80 right-0 left-0 mx-auto'>
+            <h1 className='text-3xl font-bold py-4'>Sign In</h1>
+            <input type="email" placeholder='Email address' className='p-2 m-2 bg-slate-800' />
+            <input type="password" placeholder='Password' className='p-2 m-2  bg-slate-800' />
+            <button type="button" className='p-2 m-4 rounded font-bold bg-red-600' >Sign In</button>
+            <p>New to Netflix? <span className='text-blue-500 my-2'>Sign Up</span> Now</p>
         </form>
       
     </div>
