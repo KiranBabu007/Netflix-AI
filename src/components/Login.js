9ce950dbcc8ab65aa085561fde430c051db42a9e
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-14 20:15:46 +0530

    Enhance Login component with toggle functionality for Sign In/Sign Up and update layout for better usability

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import loginbackground from '../assets/loginbackground.png'
 
 const Login = () => {
+  
+    const [SignedIn,setSignedIn]=useState(true)
+
+    const ToggleSignIn=()=>{
+        setSignedIn(!SignedIn);
+    }
+
   return (
     <div >
       <Header/>
@@ -11,12 +18,13 @@ const Login = () => {
       </div>
 
       
-        <form className='absolute gap-4 text-white p-12 bg-black w-3/12 my-36 flex flex-wrap flex-col bg-opacity-80 right-0 left-0 mx-auto'>
-            <h1 className='text-3xl font-bold py-4'>Sign In</h1>
-            <input type="email" placeholder='Email address' className='p-2 m-2 bg-slate-800' />
-            <input type="password" placeholder='Password' className='p-2 m-2  bg-slate-800' />
-            <button type="button" className='p-2 m-4 rounded font-bold bg-red-600' >Sign In</button>
-            <p>New to Netflix? <span className='text-blue-500 my-2'>Sign Up</span> Now</p>
+        <form className='absolute gap-4 text-white p-12 bg-black w-4/12 my-28 flex flex-wrap flex-col bg-opacity-80 right-0 left-0 mx-auto'>
+            <h1 className='text-4xl font-bold py-4'>{SignedIn ? "Sign In":"Sign Up"}</h1>
+            <input type="email" placeholder='Email address' className='p-2 m-2 bg-transparent bg-opacity-70 bg-slate-800' />
+            <input type="password" placeholder='Password' className='p-2 m-2  bg-transparent bg-opacity-70 bg-slate-800' />
+            <button type="button" className='p-2 m-4 rounded font-bold bg-red-600' >{SignedIn ? "Sign In":"Sign Up"}</button>
+            {SignedIn ? <p>New to Netflix? <span className='text-blue-500 my-2 cursor-pointer ' onClick={ToggleSignIn}>Sign Up</span> Now</p> : <p>Already a member? <span className='text-blue-500 my-2 cursor-pointer ' onClick={ToggleSignIn}>Sign In</span> Now</p>}
+            
         </form>
       
     </div>
