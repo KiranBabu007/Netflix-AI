89a30437a57b4bff9ce210487491c527ef437ce9
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-19 18:05:23 +0530

    Add movies slice and custom hook for fetching now playing movies; integrate with Redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,17 +4,7 @@ import { API_OPTIONS } from '../utils/constants';
 
 const Browse = () => {
 
-  const getNowPlayingMovies = async () =>{
-    
-    const data=await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTIONS);
-
-    const json=await data.json();
-    console.log(json);
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  },[])
+  
   return (
     <div>
       <Header/>
