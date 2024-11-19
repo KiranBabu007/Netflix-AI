e57d4cf0f2095385d7a96e76512575052fa63077
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-20 01:39:55 +0530

    Refactor VideoBackground and VideoTitle components for improved layout and styling; update iframe class and button styles for consistency

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import play from '../assets/play.svg'
+import React from "react";
+import play from "../assets/play.svg";
 
-const VideoTitle = ({title, desc}) => {
-
-    
-
+const VideoTitle = ({ title, desc }) => {
   return (
-    <div className='pt-36 px-12'>
-      <h1 className='text-6xl font-bold'>{title}</h1>
-      <p className='py-6 text-lg w-2/6'>{desc}</p>
-      <div className='flex'>
-        <button className='bg-gray-600 p-2 px-12 text-white text-lg opacity-55 decoration-clone rounded-md flex items-center'>
-          <img src={play} alt="Play Icon" className='mr-2 opacity-100' />
+    <div className="absolute pt-[20%] w-screen aspect-video px-12 text-white bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-2/6">{desc}</p>
+      <div className="flex">
+        <button className="hover:opacity-80 p-2 px-10 bg-white text-black text-lg rounded-md flex items-center">
+          <img src={play}  alt="Play Icon" className=" h-6 mr-2 opacity-100" />
           Play
         </button>
-        <button className='mx-2 bg-gray-600 p-2 px-12 text-white text-lg opacity-55 rounded-md'>More Info</button>
+        <button className=" hover:opacity-80 mx-2 bg-white p-2 px-10 text-black text-lg opacity-55 rounded-md">
+          More Info
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoTitle
+export default VideoTitle;
