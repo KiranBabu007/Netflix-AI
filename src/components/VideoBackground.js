6c2a8e5ad83d613441ea8c83abf852b6378374db
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-20 00:56:00 +0530

    Add trailer video fetching to VideoBackground component; integrate with Redux store to manage trailer state

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import { API_OPTIONS } from '../utils/constants'
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
-const VideoBackground = ({movieId}) => {
-  
-    const getVideo =async()=>{
-        const res = fetch('https://api.themoviedb.org/3/movie/movie_id/videos?language=en-US', API_OPTIONS)
-        const json =await res.json();
+const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+  const trailerVideo = useSelector((state) => state.movies?.trailerVideo);
 
-        const trailer =json.results.filter(video=> video.type === 'Trailer')[0];
-        
-    }
+  const getVideo = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    );
+    const json = await data.json();
 
-    useEffect(()=>{
-        getVideo();
-    },[])
+    const trailer = json.results.filter(
+      (video) => video?.type === "Trailer"
+    )[0];
+    dispatch(addTrailerVideo(trailer));
+  };
 
+  useEffect(() => {
+    getVideo();
+  }, []);
 
   return (
     <div>
-      
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
