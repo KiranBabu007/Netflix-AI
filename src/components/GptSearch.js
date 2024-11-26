bcbbdc4866269476a976e557fda57ce6647f103d
KiranBabu007 <kiranbabu3355@gmail.com>
2024-11-26 17:42:11 +0530

    Add background image and enhance GptSearchbar layout with form styling

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import GptSuggestions from './GptSuggestions'
 import GptSearchbar from './GptSearchbar'
+import loginbackground from '../assets/loginbackground.png'
 
 const GptSearch = () => {
   return (
     <div>
+      <div className="absolute inset-0 -z-10">
+        <img src={loginbackground} alt="logo" className="w-full h-full object-cover" />
+      </div>
       <GptSearchbar />
       <GptSuggestions />
     </div>
--- src/components/GptSearchbar.js
+++ src/components/GptSearchbar.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const GptSearchbar = () => {
   return (
-    <div>GptSearchbar</div>
+    <div className='flex p-[8%] justify-center'>
+      <form className=' w-1/2 bg-black grid grid-cols-12 rounded-lg'>
+        <input className='p-4 m-4 col-span-9 rounded' type="text" placeholder='What would you like to watch today?' />
+        <button className='col-span-3 py-2 m-4 px-4 text-white rounded-lg bg-red-600 hover:bg-red-700'>Search</button>
+      </form>
+    </div>
   )
 }
 
